861cc0b5ac34d5c954646dfc15178aa7f5eee0cd
ASHISH PANT <ashishpant@ashishs-macbook-pro.local>
2017-04-24 15:35:38 +0530

    change s=in delegates storyboard and view controller

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //print
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+  
+       override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
